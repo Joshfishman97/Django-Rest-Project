d390cdb4050aabafad0b974938a163776daf89f8
Joshfishman97 <fishmanjosh251@gmail.com>
2021-10-19 23:59:33 +0000

    adding models and serialiers along witha new folder and added drf to INSTALLED_APPS in settings.py

--- drfapp/models.py
+++ drfapp/models.py
@@ -5,4 +5,7 @@ class Student(model.models):
     name = models.CharField(max_length=100)
     age = models.IntergetField()
     description = models.IntergetField()
-    date_enrolled = models.DateTimeField()
+    date_enrolled = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.name
