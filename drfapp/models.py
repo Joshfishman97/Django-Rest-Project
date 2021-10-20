f57844c68114a04ab1eecd91c0bacd6e78b09a4b
Joshfishman97 <fishmanjosh251@gmail.com>
2021-10-20 16:42:01 +0000

    Added a onetoone relationship I am going to be working on more advanced projects with Django Rest next!

--- drfapp/models.py
+++ drfapp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
-    description = models.TextField()
+    description = models.TextField(blank = False, default=None, null=True)
     date_enrolled = models.DateTimeField(auto_now =True)
-
+    user = models.OneToOneField(User, on_delete = models.CASCADE, default=None, null= True)
     def __str__(self):
         return self.name
