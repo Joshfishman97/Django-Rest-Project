e8c5ba11e8324aa10362f6a49afd97f843fd6aa5
Joshfishman97 <fishmanjosh251@gmail.com>
2021-10-20 01:52:26 +0000

    Another

--- drfapp/models.py
+++ drfapp/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
-    description = models.IntegerField()
-    date_enrolled = models.DateTimeField(auto_now = True)
+    description = models.TextField()
+    date_enrolled = models.DateTimeField(auto_now =True)
 
     def __str__(self):
         return self.name
